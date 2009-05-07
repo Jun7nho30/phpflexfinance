96f2e3afd99135946997929730a6b8e14f0c2f9c
anagaychuk <anagaychuk@bc14617e-3ad5-11de-9d05-31133e6853b1>
2009-05-07 14:57:18 +0000

    Add adding account

--- trunk/application/models/DbTable/Account.php
+++ trunk/application/models/DbTable/Account.php
@@ -4,5 +4,19 @@ class Default_Model_DbTable_Account extends Zend_Db_Table_Abstract
 {
     /** Table name */
     protected $_name    = 'accounts';
+	protected $_sequence = true;
 	
+	public function insert(array $data)
+    {
+        $data['user_id'] = 1;
+		try {
+ 			return parent::insert($data);
+
+} catch (Zend_Exception $e) {
+    echo "Caught exception: " . get_class($e) . "\n";
+    echo "Message: " . $e->getMessage() . "\n";exit;
+    // Other code to recover from the error
+}
+        
+    }
 }
