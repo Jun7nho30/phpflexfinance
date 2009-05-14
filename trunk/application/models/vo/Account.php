0dad9fa59a8c220d7b00b0ee48bab29abfe854c6
anagaychuk <anagaychuk@bc14617e-3ad5-11de-9d05-31133e6853b1>
2009-05-14 05:20:11 +0000

    Add account

--- trunk/application/models/vo/Account.php
+++ trunk/application/models/vo/Account.php
@@ -1,14 +1,19 @@
 <?php 
-class Default_VO_Model_Account
+
+class Default_Model_Vo_Account
+//class AccountVO
 {
      public $id;
      public $name;
+     public $desc;     
      
      public function __construct($data = null)
      {
+     	//print_r($data);
          if ($data != null) {
              $this->id    = $data['id'];
              $this->name  = $data['name'];
+             $this->desc  = $data['desc'];
          }
      }
 }
